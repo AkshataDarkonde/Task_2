b5a5f4dd7ebdb5c2f6e2acea8d43f6fc83302466
adarkond <akshatad1234@gmail.com>
2023-11-22 18:40:24 +0530

    "Student class "

---
+++ src/main/java/com/Task2/Task2/Student.java
@@ -0,0 +1,53 @@
+package com.Task2.Task2;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Student {
+    @Id
+    String id;
+    String name;
+    int age;
+    double salary;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Student(String id, String name, int age, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
